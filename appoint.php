a2ba391fd1b16462987121469746fa95bad17964
raghadkoush <raghadkoush1@gmail.com>
2023-12-15 23:35:03 +0300

    modified:   appoint.php 	modified:   appointment.html 	modified:   ph1.php 	modified:   ph2.php 	modified:   ph3.php 	modified:   ph4.php

--- appoint.php
+++ appoint.php
@@ -1,27 +1,24 @@
 <!DOCTYPE html>
-<html>
+<!--DOCTYPE html-->
+<html >
 <head>
+    <meta charset="UTF-8">
 
+    <title>Pure Health</title>
 
+    <link rel="shortcut icon" href="logo.png">
 
-<meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    
+    <link
+      rel="stylesheet" href="login.css"  />
 
-<title>Pure Health</title>
+       <!-- Link Swiper's CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
 
-<link rel="shortcut icon" href="logo.png">
-
-<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
-
-<link
-  rel="stylesheet" href="login.css"  />
-
-   <!-- Link Swiper's CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
-
-    <style>
-        style>
+<style>
 
 *{
     margin: 0px;
@@ -112,52 +109,66 @@ body{
     color: #014dd5;
 }
 
-        table {
-            border-collapse: collapse;
-            width: 30%;
-            margin-top: 20px;
-            
-            position: relative; bottom: 60px;
-        right:260px;
-        }
-        table, th, td {
-            border: 1px solid black;
-            padding: 8px;
-  
-            color:#122853;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
 
-        td{
-    text-align:center ;
-
-}
-
-
-        
-h2{font-size:30px ;text-align:center ;margin-left:550px ; margin-right:540px;
-	position:relative;left:380px ; padding-bottom:10px; padding-top:10px ;
+p{font-size:40px ;text-align:center ;margin-left:550px ; margin-right:540px;
+	position:relative;left:370px ; padding-bottom:10px; padding-top:10px ;
+   width:500px;
     color:#122853;
+    background-color: rgba(255,255,255,0.8);
+border-radius:50px;
+
+margin-bottom:20px;
     ;}
 
 
-    </style>
+    .reset {
+              background-color: #315bb0;
+              display: inline-block;
+              text-align: center;
+              border-radius: 12px;
+              border: 2px solid rgb(173, 210, 244);
+              padding: 14px 110px;
+              outline: none;
+              color: white;
+              cursor: pointer;
+              transition: 0.2s;
+              margin: 0 10px; /* Adjusted margin */
+              margin-top: 20px;
+              position: relative;
+              left: 68%;
+          }
+
+
+    table{
+        border-collapse: collapse;
+        background-color: rgba(255,255,255,0.8);
+	width:550px;
+   height: 350px;
+   position: relative; bottom: 10px;
+   right:20%;
+
+   
+
+    }
+    th,td{
+        color:#122853;
+
+
+    }
+
+td{
+    text-align:center ;
+
+}
+</style>
 </head>
-<body>
-    
-    
-        
-<nav class="navigation">
+<body >
+   <nav class="navigation">
                 <label for="menu-btn" class="menu-icon">
                 <span class="nav-icon"></span>
             </label>
            
             <a href="" class="logo"><span>pure</span>health</a><img src="logo.png" alt="" width="80" height="80">
-            
-            
-     
     <ul class="menu">
         <li><a href="log.html"; style="font-weight: bolder;">Home  </a></li>
         <li><a href="log.html";>Find center </a></li>
@@ -168,29 +179,64 @@ h2{font-size:30px ;text-align:center ;margin-left:550px ; margin-right:540px;
 
 
     </nav>
-
-
-<?php
+    <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['firstna'];
-    $location = $_POST['location'];
-    $center = $_POST['treat'];
+    // Extracting input values from the form
+    $firstName = $_POST['firstna'] ;
+    $location = $_POST['location'] ;
+    $chosenCenter = $_POST['treat'];
 
-    echo "<h2>Appointment Details:</h2>";
+    // Handling the uploaded file
+    $uploadedFileName = isset($_FILES['fileToUpload']['name']) ? $_FILES['fileToUpload']['name'] : '';
+    $targetFile = '';
 
-   
+    // Check if a file was uploaded and it is a PDF
+    if ($_FILES['fileToUpload']['error'] === UPLOAD_ERR_OK) {
+        $fileExtension = strtolower(pathinfo($_FILES['fileToUpload']['name'], PATHINFO_EXTENSION));
 
-    echo "<table>";
-    echo "<tr><th>Patient Name</th><th>The location </th><th>The center</th></tr>";
+        if ($fileExtension !== 'pdf') {
+            echo "Error: Please upload a PDF file.";
+            // Handle the error accordingly (display error message, redirect, etc.)
+            // You might also want to consider not proceeding with further processing
+        } else {
+            // File is a PDF, proceed with moving the file
+            $targetDirectory = 'uploads/';
+            $targetFile = $targetDirectory . basename($uploadedFileName);
+            move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $targetFile);
+        }
+    } 
 
+    // Creating an array with extracted form data
+    $formData = array(
+        'Name' => $firstName,
+        'Location' => $location,
+        'Chosen Center' => $chosenCenter,
+        'Uploaded File Name' => ($targetFile !== '') ? "<a href='$targetFile' target='_blank'>$uploadedFileName</a>" : "No file"
+    );
+
+    // Displaying the extracted form data in a table
+    echo "<p> <i class='fa-solid fa-calendar-check'></i> Your Appointments</p>";
+    echo "<table border='1'>";
+    echo "<tr> <th> <i class='fa-solid fa-person'></i> Patient Name</th><th> <i class='fa-solid fa-location-dot'></i> The location </th><th> <i class='fa-regular fa-hospital'></i> The center</th> <th> <i class='fa-solid fa-upload'></i> The File </th></tr>";
     echo "<tr>";
-    echo "<td>" . $name . "</td>";
+    echo "<td>" . $firstName. "</td>";
     echo "<td>" . $location . "</td>";
-    echo "<td>" . $center. "</td>";
+    echo "<td>" . $chosenCenter. "</td>";
+    echo "<td>" . $formData['Uploaded File Name'] . "</td>";
     echo "</tr>";
-
+    echo "</table>";
 }
 ?>
 
+
+
+
+<a href="appointment.html"> <button class="reset"> Back </button></a>
+
+
+
+
+
 </body>
+
 </html>
